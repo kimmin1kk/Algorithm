c077a8cf4185b7a051b57ce12ee5391044169ba9
kimmin1kk <101302590+kimmin1kk@users.noreply.github.com>
2023-04-03 15:43:31 +0900

    Algorithm Coin 0 (11047)

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import backjoon.Coin_0;
+import backjoon.DifferentEachOtherStringCounts;
+
+import java.io.IOException;
+public class Main {
+    public static void main(String[] args) throws IOException {
+//        new Coin_0().run();
+//        new DifferentEachOtherStringCounts().run();
+
+    }
+}
---
+++ src/backjoon/Coin_0.java
@@ -0,0 +1,33 @@
+package backjoon;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Coin_0 {
+    public void run() throws IOException { //11047
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] coin = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            coin[i] = Integer.parseInt(br.readLine());
+        }
+
+        int count = 0;
+
+        for (int i = N - 1; i >= 0; i--) {
+            if (coin[i] <= K) {
+                count += (K / coin[i]);
+                K = K % coin[i];
+            }
+        }
+        bw.write(String.valueOf(count));
+        bw.flush();
+        bw.close();
+    }
+}
+
